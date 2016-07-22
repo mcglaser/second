ffa7e97a21c24994efa6daab07de4770d51c4151
mcglaser <michaelcharlesglaser@gmail.com>
2016-07-22 11:34:38 -0700

    First commit

---
+++ config/routes.rb
@@ -0,0 +1,19 @@
+Rails.application.routes.draw do
+
+  get 'sessions/new'
+
+  root 'static_pages#home'
+
+  get 'static_pages/help'
+
+  get 'signup' => 'users#new'
+
+  get 'login' => 'sessions#new'
+
+  post 'login' => 'sessions#create'
+  
+  delete 'logout' => 'sessions#destroy'
+
+  resources :users
+
+end
